f65169b712daba2da052e7a48eadefb752871f2b
olukayode <segunkayode00@gmail.com>
2026-03-02 12:00:14 +0100

    input normalization

--- 00_personal/learngo-pockets/gordle/gordle/game.go
+++ 00_personal/learngo-pockets/gordle/gordle/game.go
@@ -1,23 +1,80 @@
 package gordle
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+)
+
+// ====================
+// Helpers
+// ====================
+
+const solutionLength = 5
+
+// errInvalidWordLength is returned when the guess has the wrong number of characters.
+var errInvalidWordLength = fmt.Errorf("invalid guess, word doesn't have the same number of characters as the solution")
+
+// toUppercaseCharacters is a native implementtion to turn a string to Uppercase
+func toUppercaseCharacters(input string) []rune {
+	return []rune(strings.ToUpper(input))
+}
+
+// ====================
+// Logic
+// ====================
 
 // Game hold all the information needed to play the game Gordle
 type Game struct {
-
+	reader *bufio.Reader
 }
 
 // NewGame eturns a Game which can be used to play wht gordle game
-func NewGame() *Game {
-	g := &Game{}
+func NewGame(playerInput io.Reader) *Game {
+	g := &Game{
+		reader: bufio.NewReader(playerInput),
+	}
 
 	return g
 }
-
 
 // Play starts the gordle game
 func (g *Game) Play() {
 	fmt.Println("Welcome to Gordle")
 
 	fmt.Printf("Enter a guess:\n")
+	guess := g.ask()
+
+	fmt.Printf("Your guess is: %s\n", string(guess))
+}
+
+// ask reads input until a valid suggestion is made (and returned)
+func (g *Game) ask() []rune {
+	fmt.Printf("Enter a %d-character guess:\n", solutionLength)
+
+	for {
+		playerInput, _, err := g.reader.ReadLine()
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "Gordle failed to read your guess: %s\n", err.Error())
+			continue
+		}
+
+		guess := toUppercaseCharacters(string(playerInput))
+		if err := g.validateGuess(guess); err != nil {
+			_, _ = fmt.Printf("Your attempt is invalid with Gordle's solution: %s.\n", err.Error())
+		} else {
+			return guess
+		}
+	}
+}
+
+// validateGuess ensures the guess is valid
+func (g *Game) validateGuess(guess []rune) error {
+	if len(guess) != solutionLength {
+		return fmt.Errorf("expected %d, got %d, %w", solutionLength, len(guess), errInvalidWordLength)
+	}
+
+	return nil
 }
---
+++ 00_personal/learngo-pockets/gordle/gordle/game_internal_test.go
@@ -0,0 +1,112 @@
+package gordle
+
+import (
+	"errors"
+	"slices"
+	"strings"
+	"testing"
+)
+
+func TestGameAsk(t *testing.T) {
+	tt := map[string]struct {
+		input string
+		want  []rune
+	}{
+		"5 characters in english lowercase": {
+			input: "hello",
+			want:  []rune("HELLO"),
+		},
+		"5 characters in english uppercase": {
+			input: "HELLO",
+			want:  []rune("HELLO"),
+		},
+		"5 characters in japanese": {
+			input: "こんにちは",
+			want:  []rune("こんにちは"),
+		},
+		"3 characters in japanese": {
+			input: "こんに\nこんにちは",
+			want:  []rune("こんにちは"),
+		},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			g := NewGame(strings.NewReader(tc.input))
+
+			got := g.ask()
+			if !slices.Equal(got, tc.want) {
+				t.Errorf("got = %s, want = %s", string(got), string(tc.want))
+			}
+		})
+	}
+}
+
+func TestGameValidation(t *testing.T) {
+	tt := map[string]struct {
+		word     []rune
+		expected error
+	}{
+		"nominal": {
+			word:     []rune("GUESS"),
+			expected: nil,
+		},
+		"too long": {
+			word:     []rune("POCKET"),
+			expected: errInvalidWordLength,
+		},
+		"too short": {
+			word:     []rune("SHO"),
+			expected: errInvalidWordLength,
+		},
+		"nil character": {
+			word:     []rune{},
+			expected: errInvalidWordLength,
+		},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			g := NewGame(nil)
+
+			err := g.validateGuess(tc.word)
+			if !errors.Is(err, tc.expected) {
+				t.Errorf("%c, expected %q, got %q", tc.word, tc.expected, err)
+			}
+		})
+	}
+}
+
+func TestToUpperCase(t *testing.T) {
+	tt := map[string]struct {
+		input string
+		want  []rune
+	}{
+		"all lowercase": {
+			input: "welcome",
+			want:  []rune("WELCOME"),
+		},
+		"all uppercase": {
+			input: "JOHN",
+			want:  []rune("JOHN"),
+		},
+		"mixed cases": {
+			input: "wElCoME",
+			want:  []rune("WELCOME"),
+		},
+		"nil input": {
+			input: "",
+			want:  []rune{},
+		},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			got := toUppercaseCharacters(tc.input)
+
+			if !slices.Equal(got, tc.want) {
+				t.Errorf("expected: %v, got %v", tc.want, got)
+			}
+		})
+	}
+}
