52804263b5aac717ecdfa3b2669c53414614ba47
olukayode <segunkayode00@gmail.com>
2026-03-04 15:46:35 +0100

    setup decimal

--- 00_personal/learngo-pockets/moneyconverter/main.go
+++ 00_personal/learngo-pockets/moneyconverter/main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"math"
+)
+
 func main() {
-	
+	fmt.Println(math.Abs(math.Sin(math.Pi) - 0))
+	fmt.Println(math.Pow(10, -15))
 }
--- 00_personal/learngo-pockets/moneyconverter/money/decimal.go
+++ 00_personal/learngo-pockets/moneyconverter/money/decimal.go
@@ -1,4 +1,33 @@
 package money
 
+import (
+	"strconv"
+	"strings"
+)
+
+// Decimal can represent a floating-point number with a fixed precision.
+// example: 1.52 = 152 * 10^(-2) and will be stored as {152, 2}
 type Decimal struct {
+	// subunits is the amount of subunits.
+	// Multiply it by the precision to get th real value
+	subunits  int64
+	precision byte
+}
+
+func (d Decimal) ParseDecimal(str string) (Decimal, error) {
+	before, after, ok := strings.Cut(str, ".")
+	if !ok {
+		before = str
+		after = ""
+	}
+
+	num, err := strconv.ParseInt(before+after, 10, 64)
+	if err != nil {
+		return Decimal{}, nil
+	}
+
+	return Decimal{
+		subunits:  num,
+		precision: byte(len(after)),
+	}, nil
 }
